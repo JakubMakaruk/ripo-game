3db6718bd73c2d04ade35aa6129dd627ddf2b0b2
Jakub Makaruk <36269487+JakubMakaruk@users.noreply.github.com>
2021-03-21 15:44:54 +0100

    Fix not repeat words Breakpoint #1 (list)

--- Racing Improving Polish Orthography/Assets/Scripts/Player.cs
+++ Racing Improving Polish Orthography/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,26 +21,36 @@ public class Player : MonoBehaviour
     private Color greenColor = new Color32(86, 229, 25, 255);
     private Color redColor = new Color32(229, 26, 30, 255);
 
-    private int currentAnswer;
+    private int currentAnswer; // 10 - U , 11 - Ó
     private int counterWords;
     private string currentWord;
     private int randomStartValue; // 0 - words_u , 1 - words_o
     private int counterAll = 0, correctCounter = 0, wrongCounter = 0;
+    private int randomValue;
 
-    private string[] words_u;
-    private string[] words_o;
+    //private string[] words_u;
+    //private string[] words_o;
     private List<string> words_appeared;
+
+    private List<string> _words_u;
+    private List<string> _words_o;
 
     void Start()
     {
         words_appeared = new List<string>();
+        _words_u = new List<string>();
+        _words_o = new List<string>();
 
-        words_u = File.ReadAllLines(filename_u);
-        words_o = File.ReadAllLines(filename_o);
-        for (int i = 0; i < words_u.Length; i++)
+        //words_u = File.ReadAllLines(filename_u);
+        //words_o = File.ReadAllLines(filename_o);
+
+        _words_u = File.ReadAllLines(filename_u).ToList<string>();
+        _words_o = File.ReadAllLines(filename_o).ToList<string>();
+
+        /*for (int i = 0; i < words_u.Length; i++)
         {
             Debug.Log(words_u[i]);
-        }
+        }*/
 
         correctAnswersCounter.text = "0";
         wrongAnswersCounter.text = "0";
@@ -74,7 +85,8 @@ public class Player : MonoBehaviour
             correctAnswersCounter.text = correctCounter.ToString();
 
             DestroySpawnLetter(counterAll);
-            words_appeared.Add(currentWord); // add current word to list to not repeat if answered correctly
+
+            RemoveWordFromList(currentWord, randomValue, currentAnswer);
 
             yield return new WaitForSeconds(2);
             GenerateNewWord();
@@ -98,25 +110,22 @@ public class Player : MonoBehaviour
 
     private void GenerateNewWord()
     {
-        do
+        System.Random random = new System.Random();
+        int randomStartValue = random.Next(0, 2);
+        Debug.Log("Random start " + randomStartValue.ToString());
+        if (randomStartValue == 0)
         {
-            System.Random random = new System.Random();
-            int randomStartValue = random.Next(0, 2);
-            Debug.Log("Random start " + randomStartValue.ToString());
-            if (randomStartValue == 0)
-            {
-                int randomValue = random.Next(0, words_u.Length);
-                textObject.text = currentWord = words_u[randomValue];
-                currentAnswer = 10;
-            }
-            else
-            {
-                int randomValue = random.Next(0, words_o.Length);
-                textObject.text = currentWord = words_o[randomValue];
-                currentAnswer = 11;
-            }
+            randomValue = random.Next(0, _words_u.Count);
+            textObject.text = currentWord = _words_u[randomValue];
+            currentAnswer = 10;
+        }
+        else
+        {
+            randomValue = random.Next(0, _words_o.Count);
+            textObject.text = currentWord = _words_o[randomValue];
+            currentAnswer = 11;
+        }
             
-        } while (IfWordAppeared(currentWord));
         counterAll++;
         textObject.color = yellowColor;
     }
@@ -127,13 +136,20 @@ public class Player : MonoBehaviour
         Destroy(spawnersLetters[index-1].gameObject);
     }
 
-    private bool IfWordAppeared(string word)
+    private void RemoveWordFromList(string word, int index, int letter)
     {
-        foreach(string it in words_appeared)
+        switch (letter)
         {
-            Debug.Log("AAAAA: " + it);
+            case 10:
+                Debug.Log(_words_u.Count.ToString() + "\n" + index.ToString() + " " + word);
+                _words_u.RemoveAt(index);
+                Debug.Log("USUNIĘTO!");
+                break;
+            case 11:
+                Debug.Log(_words_o.Count.ToString() + "\n" + index.ToString() + " " + word);
+                _words_o.RemoveAt(index);
+                Debug.Log("USUNIĘTO!");
+                break;
         }
-        Debug.Log(words_appeared.Contains(word));
-        return words_appeared.Contains(word);
     }
 }
