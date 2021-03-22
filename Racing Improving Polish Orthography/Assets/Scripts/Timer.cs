63bca80b09b5b9772574800ae442bb14873f6884
Jakub Makaruk <36269487+JakubMakaruk@users.noreply.github.com>
2021-03-22 21:03:13 +0100

    Add timer

---
+++ Racing Improving Polish Orthography/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI textTimer;
+
+    private int minutes, seconds, miliseconds;
+
+    void Start()
+    {
+        //minutes = seconds = miliseconds = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayTime();
+    }
+
+    void DisplayTime()
+    {
+        minutes = (int)(Time.time / 60f);
+        seconds = (int)(Time.time % 60f);
+        miliseconds = (int)(Time.time * 100f)%100;
+        //miliseconds = (int)(Time.time / 3600000.0f);
+        textTimer.text = minutes.ToString("D2") + ":" + seconds.ToString("D2") + ":" + miliseconds.ToString("D2");//string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, miliseconds);
+        //Debug.Log(minutes.ToString() + ":" + seconds.ToString() + ":" + miliseconds.ToString());
+    }
+}
